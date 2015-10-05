cbe7845d5993c5aebb9d17187ce290e84cc5b536
Nakata Nobuyuki <nobu.nakata@gmail.com>
2015-10-05 20:00:32 +0900

    no message

---
+++ Assets/Examples/Physics/WallRotator.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallRotator : MonoBehaviour {
+	public Vector3 speed;
+
+	void Update () {
+		transform.localRotation *= Quaternion.Euler(speed * Time.deltaTime);
+	}
+}
